3ecfea52cbaec7b6babe77a5340eec56b7b295dd
Henrique Jonas <henrique@2x3.com.br>
2019-12-23 19:46:15 -0300

    WIP - Fix encode with accent char hashids

--- spotsales-utils/src/main/java/br/com/spotsales/utils/SecurityUtils.kt
+++ spotsales-utils/src/main/java/br/com/spotsales/utils/SecurityUtils.kt
@@ -2,20 +2,29 @@ package br.com.spotsales.utils
 
 import org.hashids.Hashids
 import org.mindrot.jbcrypt.BCrypt
-import java.nio.ByteBuffer
 import java.util.*
+import kotlin.math.absoluteValue
 
 object SecurityUtils {
 
     fun hashidsEncode(code: String, salt: String): String {
-        val hashids = Hashids(salt)
-        val longList = code.toByteArray().map { i -> i.toLong() }.toLongArray()
-        return hashids.encode(*longList)
+        val minValue = Byte.MIN_VALUE.toLong().absoluteValue
+        val longList =
+            code.toByteArray()
+                .map { i ->
+                    if (i < 0) {
+                        minValue * 2 + i.toLong()
+                    } else {
+                        i.toLong()
+                    }
+                }
+                .toLongArray()
+
+        return Hashids(salt).encode(*longList)
     }
 
     fun hashidsDecode(code: String, salt: String): String {
-        val hashids = Hashids(salt)
-        val result = hashids.decode(code)
+        val result = Hashids(salt).decode(code)
         val bytes = ByteArray(result.size)
 
         (result.indices).forEach { bytes[it] = result[it].toByte() }
